0652a3fcfaed6676340db63c4d87e2fa407d1710
Husaam NT <husnaeem@gmail.com>
2022-11-27 19:06:34 +0000

    added questions array and inquirer function

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
+const fs = require("fs")
+const inquirer = require("inquirer")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Name",
+    "Description",
+    "Installation Instructions",
+    "Usage Information",
+    "Contribution Guidlines",
+    "Test Instructions",
+];
+const inquirerQuestions = questions.map(it => {
+    return{
+        type: "input",
+        name: "name",
+        message: "Input your name:"
+
+    },
+}
+    )
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +29,10 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+
+
+
